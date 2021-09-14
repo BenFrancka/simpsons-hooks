feb6135a580c93e7c2b2d580ea8209ad26638707
BenFrancka <benfrancka@gmail.com>
2021-09-13 17:56:00 -0700

    adds quote container

---
+++ src/containers/quote/QuoteContainer.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import LoadQuoteButton from '../../components/quote/QuoteControl';
+import QuoteDisplay from '../../components/quote/QuoteDisplay';
+import { fetchQuote } from '../../services/simpsonsApi';
+
+const SimpsonsQuote = () => {
+  const [quote, setQuote] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleClick = async () => {
+    setLoading(true);
+    const quote = await fetchQuote();
+    setQuote(quote);
+    setLoading(false);
+  };
+
+  return (
+    <>
+      <LoadQuoteButton onClick={handleClick} />
+      {loading ? (
+        <img
+          // eslint-disable-next-line max-len
+          src="https://giphy.com/gifs/thesimpsons-the-simpsons-3x14-xT5LMzIK1AdZJ4cYW4"
+          alt="loading spinner"
+        />
+      ) : (
+        quote && (
+          <QuoteDisplay
+            quote={quote.quote}
+            image={quote.image}
+            character={quote.character}
+          />
+        )
+      )}
+    </>
+  );
+};
+
+export default SimpsonsQuote;
+
